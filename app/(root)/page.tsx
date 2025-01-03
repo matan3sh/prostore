ba8f1f44758a5425897baae3768a9b3dbea57876
Matan <matan3sh@gmail.com>
2025-01-03 14:54:49 +0200

    Sample Products & Product List

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,15 @@
+import ProductList from '@/components/shared/product/product-list'
+import sampleData from '@/db/sample-data'
+
 const HomePage = () => {
-  return <>ProStore</>
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  )
 }
 export default HomePage
---
+++ components/shared/product/product-list.tsx
@@ -0,0 +1,28 @@
+interface Props {
+  data: any
+  limit?: number
+  title?: string
+}
+
+const ProductList = ({ data, limit, title }: Props) => {
+  const limitedData = limit ? data.slice(0, limit) : data
+
+  return (
+    <div className="my-10">
+      <h2 className="h2-bold mb-4">{title}</h2>
+      {data.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {limitedData.map((product: any) => (
+            <div>{product.name}</div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No products found</p>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ProductList
