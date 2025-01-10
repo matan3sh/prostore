7fab163331180542bdfcd59527166fc5dda9471b
Matan <matan3sh@gmail.com>
2025-01-10 18:47:07 +0200

    Hook Up Sign In Form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,4 +1,5 @@
 import CredentialsSignInForm from '@/app/(auth)/sign-in/credentials-signin-form'
+import { auth } from '@/auth'
 import {
   Card,
   CardContent,
@@ -10,12 +11,19 @@ import { APP_NAME } from '@/lib/constants'
 import { Metadata } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
 export const metadata: Metadata = {
   title: 'Sign In',
 }
 
-const SignInPage = () => {
+const SignInPage = async () => {
+  const session = await auth()
+
+  if (session) {
+    return redirect('/')
+  }
+
   return (
     <div className="w-full max-w-md mx-auto">
       <Card>
