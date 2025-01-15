176991536e886570734eca8820c765f0e6e68f1b
Matan <matan3sh@gmail.com>
2025-01-15 10:05:23 +0200

    ShadCN UI Table

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -1,9 +1,20 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 import { useToast } from '@/hooks/use-toast'
-import { Cart } from '@/types'
+import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions'
+import { Cart, CartItem } from '@/types'
+import { Loader, Minus, Plus } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
 import { useTransition } from 'react'
 
 interface Props {
@@ -11,9 +22,35 @@ interface Props {
 }
 
 const CartTable = ({ cart }: Props) => {
-  const router = useRouter()
+  // const router = useRouter()
   const { toast } = useToast()
   const [isPending, startTransition] = useTransition()
+
+  const handleRemoveItemFromCart = (productId: string) => {
+    startTransition(async () => {
+      const res = await removeItemFromCart(productId)
+
+      if (!res.success) {
+        toast({
+          variant: 'destructive',
+          description: res.message,
+        })
+      }
+    })
+  }
+
+  const handleAddItemToCart = (item: CartItem) => {
+    startTransition(async () => {
+      const res = await addItemToCart(item)
+
+      if (!res.success) {
+        toast({
+          variant: 'destructive',
+          description: res.message,
+        })
+      }
+    })
+  }
 
   return (
     <>
@@ -24,7 +61,65 @@ const CartTable = ({ cart }: Props) => {
         </div>
       ) : (
         <div className="grid md:grid-cols-4 md:gap-5">
-          <div className="overflow-x-auto md:col-span-3">Table</div>
+          <div className="overflow-x-auto md:col-span-3">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Item</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-right">Price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {cart.items.map((item) => (
+                  <TableRow key={item.slug}>
+                    <TableCell>
+                      <Link
+                        href={`/product/${item.slug}`}
+                        className="flex items-center"
+                      >
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        />
+                        <span className="px-2">{item.name}</span>
+                      </Link>
+                    </TableCell>
+                    <TableCell className="flex-center gap-2">
+                      <Button
+                        disabled={isPending}
+                        variant={'outline'}
+                        type={'button'}
+                        onClick={() => handleRemoveItemFromCart(item.productId)}
+                      >
+                        {isPending ? (
+                          <Loader className="w-4 h-4 animate-spin" />
+                        ) : (
+                          <Minus className="w-4 h-4" />
+                        )}
+                      </Button>
+                      <span>{item.quantity}</span>
+                      <Button
+                        disabled={isPending}
+                        variant={'outline'}
+                        type={'button'}
+                        onClick={() => handleAddItemToCart(item)}
+                      >
+                        {isPending ? (
+                          <Loader className="w-4 h-4 animate-spin" />
+                        ) : (
+                          <Plus className="w-4 h-4" />
+                        )}
+                      </Button>
+                    </TableCell>
+                    <TableCell className="text-right">${item.price}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
         </div>
       )}
     </>
