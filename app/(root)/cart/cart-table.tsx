6ca79c623092bf509bf579aa7837a127703e794f
Matan <matan3sh@gmail.com>
2025-01-15 09:43:17 +0200

    Cart Page

---
+++ app/(root)/cart/cart-table.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { useToast } from '@/hooks/use-toast'
+import { Cart } from '@/types'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useTransition } from 'react'
+
+interface Props {
+  cart?: Cart
+}
+
+const CartTable = ({ cart }: Props) => {
+  const router = useRouter()
+  const { toast } = useToast()
+  const [isPending, startTransition] = useTransition()
+
+  return (
+    <>
+      <h1 className="py-4 h2-bold">Shopping Cart</h1>
+      {!cart || cart.items.length === 0 ? (
+        <div>
+          Cart is empty. <Link href="/">Go Shopping</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">Table</div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default CartTable
---
+++ app/(root)/cart/page.tsx
@@ -0,0 +1,18 @@
+import CartTable from '@/app/(root)/cart/cart-table'
+import { getMyCart } from '@/lib/actions/cart.actions'
+
+export const metadata = {
+  title: 'Shopping Cart',
+}
+
+const CartPage = async () => {
+  const cart = await getMyCart()
+
+  return (
+    <>
+      <CartTable cart={cart} />
+    </>
+  )
+}
+
+export default CartPage
