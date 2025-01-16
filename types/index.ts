442342e3e443042f088bb4806ff990d3eb264cde
Matan <matan3sh@gmail.com>
2025-01-16 19:45:39 +0200

    Order Zod Schema & Types

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,11 @@
 import {
   cartItemSchema,
   insertCartSchema,
+  insertOrderItemSchema,
+  insertOrderSchema,
   insertProductSchema,
   paymentMethodSchema,
+  paymentResultSchema,
   shippingAddressSchema,
 } from '@/lib/validators'
 import { z } from 'zod'
@@ -20,3 +23,19 @@ export type CartItem = z.infer<typeof cartItemSchema>
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>
 
 export type PaymentMethod = z.infer<typeof paymentMethodSchema>
+
+export type OrderItem = z.infer<typeof insertOrderItemSchema>
+
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string
+  createdAt: Date
+  isPaid: boolean
+  paidAt: Date | null
+  isDelivered: boolean
+  deliveredAt: Date | null
+  orderitems: OrderItem[]
+  user: { name: string; email: string }
+  paymentResult: PaymentResult
+}
+
+export type PaymentResult = z.infer<typeof paymentResultSchema>
