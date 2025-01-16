078e81700e5267a930cec92459ccd3cead7162f2
Matan <matan3sh@gmail.com>
2025-01-16 17:34:47 +0200

    Payment Method Form & Update

---
+++ app/(root)/payment-method/page.tsx
@@ -0,0 +1,27 @@
+import PaymentMethodForm from '@/app/(root)/payment-method/payment-method-form'
+import { auth } from '@/auth'
+import CheckoutSteps from '@/components/shared/checkout-steps'
+import { getUserById } from '@/lib/actions/user.actions'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Select Payment Method',
+}
+
+const PaymentMethodPage = async () => {
+  const session = await auth()
+  const userId = session?.user?.id
+
+  if (!userId) throw new Error('User not found')
+
+  const user = await getUserById(userId)
+
+  return (
+    <>
+      <CheckoutSteps current={2} />
+      <PaymentMethodForm preferredPaymentMethod={user.paymentMethod} />
+    </>
+  )
+}
+
+export default PaymentMethodPage
