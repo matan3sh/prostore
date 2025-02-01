56e1d16c31f0aea58ae663764a6f4fe4ff9d9589
Matan <matan3sh@gmail.com>
2025-02-01 19:35:36 +0200

    Approve & Update Order

--- lib/actions/order/order.actions.ts
+++ lib/actions/order/order.actions.ts
@@ -6,7 +6,7 @@ import { getMyCart } from '@/lib/actions/cart.actions'
 import { getUserById } from '@/lib/actions/user.actions'
 import { convertToPlainObject, formatError } from '@/lib/utils'
 import { insertOrderSchema } from '@/lib/validators'
-import { CartItem } from '@/types'
+import { CartItem, PaymentResult } from '@/types'
 import { isRedirectError } from 'next/dist/client/components/redirect-error'
 
 // Create order and create the order items
@@ -108,4 +108,67 @@ export async function getOrderById(orderId: string) {
     },
   })
   return convertToPlainObject(data)
+}
+
+// Update order to paid
+export async function updateOrderToPaid({
+  orderId,
+  paymentResult,
+}: {
+  orderId: string
+  paymentResult?: PaymentResult
+}) {
+  // Get order from database
+  const order = await prisma.order.findFirst({
+    where: {
+      id: orderId,
+    },
+    include: {
+      orderitems: true,
+    },
+  })
+
+  if (!order) throw new Error('Order not found')
+
+  if (order.isPaid) throw new Error('Order is already paid')
+
+  // Transaction to update order and account for product stock
+  await prisma.$transaction(async (tx) => {
+    // Iterate over products and update stock
+    for (const item of order.orderitems) {
+      await tx.product.update({
+        where: { id: item.productId },
+        data: { stock: { increment: -item.qty } },
+      })
+    }
+
+    // Set the order to paid
+    await tx.order.update({
+      where: { id: orderId },
+      data: {
+        isPaid: true,
+        paidAt: new Date(),
+        paymentResult,
+      },
+    })
+  })
+
+  // Get updated order after transaction
+  const updatedOrder = await prisma.order.findFirst({
+    where: { id: orderId },
+    include: {
+      orderitems: true,
+      user: { select: { name: true, email: true } },
+    },
+  })
+
+  if (!updatedOrder) throw new Error('Order not found')
+
+  // sendPurchaseReceipt({
+  //   order: {
+  //     ...updatedOrder,
+  //     shippingAddress: updatedOrder.shippingAddress as ShippingAddress,
+  //     paymentResult: updatedOrder.paymentResult as PaymentResult,
+  //   },
+  // });
 }
--- lib/actions/order/paypal.order.actions.ts
+++ lib/actions/order/paypal.order.actions.ts
@@ -1,8 +1,11 @@
 'use server'
 
 import { prisma } from '@/db/prisma'
+import { updateOrderToPaid } from '@/lib/actions/order/order.actions'
 import { paypal } from '@/lib/paypal'
 import { formatError } from '@/lib/utils'
+import { PaymentResult } from '@/types'
+import { revalidatePath } from 'next/cache'
 
 // Create new paypal order
 export async function createPayPalOrder(orderId: string) {
@@ -38,6 +41,54 @@ export async function createPayPalOrder(orderId: string) {
       }
     } else {
       throw new Error('Order not found')
+    }
+  } catch (error) {
+    return { success: false, message: formatError(error) }
+  }
+}
+
+// Approve paypal order and update order to paid
+export async function approvePayPalOrder(
+  orderId: string,
+  data: { orderID: string }
+) {
+  try {
+    // Get order from database
+    const order = await prisma.order.findFirst({
+      where: {
+        id: orderId,
+      },
+    })
+
+    if (!order) throw new Error('Order not found')
+
+    const captureData = await paypal.capturePayment(data.orderID)
+
+    if (
+      !captureData ||
+      captureData.id !== (order.paymentResult as PaymentResult)?.id ||
+      captureData.status !== 'COMPLETED'
+    ) {
+      throw new Error('Error in PayPal payment')
+    }
+
+    // Update order to paid
+    await updateOrderToPaid({
+      orderId,
+      paymentResult: {
+        id: captureData.id,
+        status: captureData.status,
+        email_address: captureData.payer.email_address,
+        pricePaid:
+          captureData.purchase_units[0]?.payments?.captures[0]?.amount?.value,
+      },
+    })
+
+    revalidatePath(`/order/${orderId}`)
+
+    return {
+      success: true,
+      message: 'Your order has been paid',
     }
   } catch (error) {
     return { success: false, message: formatError(error) }
