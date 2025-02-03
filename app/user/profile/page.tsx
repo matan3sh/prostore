cf1d72d0b1f2e6e0793f32a6197a4c2aec7c3ca7
Matan <matan3sh@gmail.com>
2025-02-03 19:36:56 +0200

    User Layout & Menu

---
+++ app/user/profile/page.tsx
@@ -0,0 +1,22 @@
+import { auth } from '@/auth'
+import { Metadata } from 'next'
+import { SessionProvider } from 'next-auth/react'
+
+export const metadata: Metadata = {
+  title: 'Customer Profile',
+}
+
+const Profile = async () => {
+  const session = await auth()
+
+  return (
+    <SessionProvider session={session}>
+      <div className="max-w-md mx-auto space-y-4">
+        <h2 className="h2-bold">Profile</h2>
+        Profile Form
+      </div>
+    </SessionProvider>
+  )
+}
+
+export default Profile
