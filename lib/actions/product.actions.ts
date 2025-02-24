c50e49d5094ee1a02653b932dd7315a522511ff8
Matan <matan3sh@gmail.com>
2025-02-24 21:11:56 +0200

    Add Product Filtering and Deletion Actions in Admin Dashboard

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,8 +1,10 @@
 'use server'
 
 import { prisma } from '@/db/prisma'
-import { LATEST_PRODUCTS_LIMIT } from '@/lib/constants'
-import { convertToPlainObject } from '@/lib/utils'
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from '@/lib/constants'
+import { convertToPlainObject, formatError } from '@/lib/utils'
+import { Prisma } from '@prisma/client'
+import { revalidatePath } from 'next/cache'
 
 // Get latest products
 export async function getLatestProducts() {
@@ -25,4 +27,106 @@ export async function getProductBySlug(slug: string) {
   })
 
   return product
+}
+
+// Get all products
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+  price,
+  rating,
+  sort,
+}: {
+  query: string
+  limit?: number
+  page: number
+  category?: string
+  price?: string
+  rating?: string
+  sort?: string
+}) {
+  // Query filter
+  const queryFilter: Prisma.ProductWhereInput =
+    query && query !== 'all'
+      ? {
+          name: {
+            contains: query,
+            mode: 'insensitive',
+          } as Prisma.StringFilter,
+        }
+      : {}
+
+  // Category filter
+  const categoryFilter = category && category !== 'all' ? { category } : {}
+
+  // Price filter
+  const priceFilter: Prisma.ProductWhereInput =
+    price && price !== 'all'
+      ? {
+          price: {
+            gte: Number(price.split('-')[0]),
+            lte: Number(price.split('-')[1]),
+          },
+        }
+      : {}
+
+  // Rating filter
+  const ratingFilter =
+    rating && rating !== 'all'
+      ? {
+          rating: {
+            gte: Number(rating),
+          },
+        }
+      : {}
+
+  const data = await prisma.product.findMany({
+    where: {
+      ...queryFilter,
+      ...categoryFilter,
+      ...priceFilter,
+      ...ratingFilter,
+    },
+    orderBy:
+      sort === 'lowest'
+        ? { price: 'asc' }
+        : sort === 'highest'
+        ? { price: 'desc' }
+        : sort === 'rating'
+        ? { rating: 'desc' }
+        : { createdAt: 'desc' },
+    skip: (page - 1) * limit,
+    take: limit,
+  })
+
+  const dataCount = await prisma.product.count()
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  }
+}
+
+// Delete a product
+export async function deleteProduct(id: string) {
+  try {
+    const productExists = await prisma.product.findFirst({
+      where: { id },
+    })
+
+    if (!productExists) throw new Error('Product not found')
+
+    await prisma.product.delete({ where: { id } })
+
+    revalidatePath('/admin/products')
+
+    return {
+      success: true,
+      message: 'Product deleted successfully',
+    }
+  } catch (error) {
+    return { success: false, message: formatError(error) }
+  }
 }
